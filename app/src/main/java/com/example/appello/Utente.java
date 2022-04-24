1afd0bd0c95b1f6428556dca7a23df3b0eab1b1e
Carolina Pellecchia <100867346+carpelGithub@users.noreply.github.com>
2022-04-24 14:27:09 +0200

    Add files via upload
    Prima versione degli alunni Carmen e Alessandro 4C 2022

---
+++ app/src/main/java/com/example/appello/Utente.java
@@ -0,0 +1,13 @@
+package com.example.appello;
+
+public class Utente {
+
+    public String nome, cognome, email, pass ;
+
+    public Utente(String nome, String cognome, String email){
+
+        this.nome = nome ;
+        this.cognome = cognome ;
+        this.email = email ;
+    }
+}
